0a76a0f025542d49c6000f6fbdca0cfc4d38fa64
nqwzxs <nqwzxs@gmail.com>
2023-07-04 12:22:41 +0500

    Add location search input listener

--- src/index.js
+++ src/index.js
@@ -1,8 +1,47 @@
+const searchLocationForm = document.querySelector('form');
+
+searchLocationForm.addEventListener('submit', async (e) => {
+  e.preventDefault();
+
+  const input = document.querySelector('input');
+  const location = input.value;
+
+  const weatherData = await getWeatherData(location);
+  updateWeatherCard(weatherData);
+});
+
 async function getWeatherData(location) {
-  const url = `http://api.weatherapi.com/v1/current.json?key=c8360e2b8d134939891121004232706&q=${location}`;
+  const apiKey = 'c8360e2b8d134939891121004232706';
+  const url = 'http://api.weatherapi.com/v1/current.json?key=' + apiKey + '&q=$' + location;
   const response = await fetch(url);
-  const weatherData = await response.json();
-  console.log(weatherData);
+  
+  if (response.status === 200) {
+    return response.json();
+  }
 }
 
-getWeatherData('london')
+async function getGif(search) {
+  const apiKey = 'AIzaSyBPMeUcdifnjv8NE9f_T-JJrJRLwIWYf4A';
+  const url = "https://tenor.googleapis.com/v2/search?q=" + `weather_${search}` + "&key=" + apiKey;
+  const response = await fetch(url);
+
+  if (response.status === 200) {
+    const data = await response.json();
+    const gifs = data.results;
+    
+    const gif = gifs[Math.floor(Math.random() * gifs.length)];
+    const gifUrl = gif.media_formats.gif.url;
+
+    console.log(gifUrl);
+  }
+}
+
+function updateWeatherCard(data) {
+  const location = document.querySelector('.location');
+  const temperature = document.querySelector('.temperature');
+  const condition = document.querySelector('.condition');
+
+  location.textContent = `${data.location.name}, ${data.location.country}`;
+  temperature.textContent = `${data.current.temp_c}°C`;
+  condition.textContent = data.current.condition.text;
+}
