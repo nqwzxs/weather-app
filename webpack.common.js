5d3f5687809bbf343688b3026f063ad7ab91f6d5
nqwzxs <nqwzxs@gmail.com>
2023-07-04 10:00:26 +0500

    Add HTML and CSS to weather app

--- webpack.common.js
+++ webpack.common.js
@@ -1,26 +1,9 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Weather App',
-    }),
-  ],
+  entry: './src/index.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
   },
 };
